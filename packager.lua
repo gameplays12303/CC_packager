fc1f935b763d182275e738a443a5e4b8abdd2857
wendell_Parham <wendell.parham@outlook.com>
2023-07-10 21:01:16 -0500

    Add files via upload

--- packager.lua
+++ packager.lua
@@ -1,128 +1,111 @@
 local expect = require("cc.expect").expect
 local util = require("modules.utilties")
 local fm  = require("modules.fm")
+local pretty = require("cc.pretty")
 local argue = {...}
+--the Path to package up
 local LUA_PATH = expect(1,argue[1],"string")
-local LUA_WRITE = expect(2,argue[2],"string")
-LUA_WRITE = util.file.withoutExtension(LUA_WRITE)..".Lins"
-local LUA_isRealitive = false
-if argue[3] and (argue[3] == "true" or argue[3] == true)
+-- the installer Path
+local LUA_WRITE = expect(2,argue[2],"string") or ""
+LUA_WRITE = util.file.withoutExtension(LUA_WRITE)..".LINS"
+-- allows to specifi a installation Path when installing 
+-- example "../build/OS" --> "../OS"
+-- the 'build' directory  is left out because it's not a part of the program just where it's stored
+-- set to true to make it a install anyWhere file 
+local LUA_Install = expect(3,argue[3],"string","number","boolean","nil")
+if LUA_Install and LUA_Install == "false"
 then
-    LUA_isRealitive = true
-end
-if not fs.exists(LUA_PATH)
+    LUA_Install = false
+elseif LUA_Install and LUA_Install == "true"
 then
-    error(("%s : #1 directory is nonExistant"):format(LUA_PATH),0)
-end
-if not fs.isDir(LUA_PATH)
+    LUA_Install = true
+elseif LUA_Install
 then
-    error(("%s: #1 expected directory got File"):format(LUA_PATH),3)
- end
-if fs.isDir(LUA_WRITE)
-then
-    error(("%s: #2 expected file type got directory type"):format(LUA_WRITE),3)
-end
-local files = util.file.listsubs(LUA_PATH,true,false,false,false)
-local mainTable = {}
-local Dirs = util.file.listsubs(LUA_PATH,false,true,true,false)
-local DirsMainTable = {}
-printError("files")
-for i,v in pairs(files) do
-    textutils.slowPrint(v)
-end
-printError("directorys")
-for i,v in pairs(Dirs) do
-    textutils.slowPrint(v)
-end
-if LUA_isRealitive
-then
-    for a,v in pairs(files) do
-        local Temp = util.string.split(v,"/")
-        local Path = ""
-        for i,b in pairs(Temp) do
-            if b == fs.getName(LUA_PATH)
-            then
-                Path = table.concat(Temp,"/",i,#Temp)
-            end
-        end
-        files[a] = Path
-    end
-    for a,v in pairs(Dirs) do
-        local Temp = util.string.split(v,"/")
-        local Path = ""
-        for i,b in pairs(Temp) do
-            if b == fs.getName(LUA_PATH)
-            then
-                Path = table.concat(Temp,"/",i,#Temp)
-            end
-        end
-        Dirs[a] = Path
-    end
-end
-printError("loading")
-for _,v in pairs(files) do
-    textutils.slowPrint(v)
-    local Path = ""
-    local Table = {
-        Path = v
-    }
-    if LUA_isRealitive
+    LUA_Install = tonumber(LUA_Install)
+    if type(LUA_Install) == "number"
     then
-        Table.func = fm.readFile(fs.combine(fs.getDir(LUA_PATH),v),"R")
-    else
-        Table.func = fm.readFile(v,"R")
+        LUA_Install = LUA_Install +1
     end
-    table.insert(mainTable,Table)
 end
-for _,v in pairs(Dirs) do
-    local Table = {}
-    Table.Path = v
-    table.insert(DirsMainTable,Table)
+-- gets a list of programs and directorys to be packaged
+local Dir = util.file.listsubs(LUA_PATH,false,true,true,false)
+local files = util.file.listsubs(LUA_PATH,true,false,false,false)
+local sDir,sFiles = {},{}
+if LUA_Install and type(LUA_Install) == "number"
+then
+    for _,v in pairs(Dir) do
+        table.insert(sDir,util.string.split(v,"%/"))
+    end
+    for _,v in pairs(files) do
+        table.insert(sFiles,util.string.split(v,"%/"))
+    end
+
+    for i,v in pairs(sDir) do
+        sDir[i] = table.concat(v,"/",LUA_Install)
+    end
+    for i,v in pairs(sFiles) do
+        sFiles[i] = table.concat(v,"/",LUA_Install)
+    end
+else
+    sDir = Dir
+    sFiles = files
 end
 
+for i,v in pairs(sFiles) do
+    local temp = {
+        Path = v,
+        func = fm.readFile(files[util.table.find(files,v)],"S")
+    }
+    sFiles[i] = temp
+end
+-- time to build the run handler
 local runhanndle = [[
-    local Argue = {...}
-    local expect = require("cc.expect").expect
-    local Path
-    if %q
+local Argue = {...}
+local expect = require("cc.expect").expect
+local Path
+if %q
+then
+    Path = expect(1,Argue[1],"string","nil") or ""
+    if not fs.exists(Path) 
     then
-        Path = expect(1,Argue[1],"string","nil") or ""
-        if not fs.exists(Path) 
-        then
-            error(("%s not found"):format(Path),2)
-        end
-        if not fs.isDir(Path)
-        then
-            error(("%s is not directory"):format(Path),2)
-        end
+        error(("%s not found"):format(Path),2)
     end
-    local programs = textutils.unserialise(%q)
-    local Dirs = %s
-    for _,v in pairs(Dirs) do
-        if Path
-        then
-            fs.makeDir(fs.combine(Path,v.Path))
-        else
-            fs.makeDir(v.Path)
-        end
+    if not fs.isDir(Path)
+    then
+        error(("%s is not directory"):format(Path),2)
     end
-    for _,v in pairs(programs) do
-        local file,mess
-        if Path
-        then
-            file,mess = fs.open(fs.combine(Path,v.Path),"w")
-        else
-            file,mess = fs.open(v.Path,"w")
-        end
-        if not file
-        then
-            error(mess,0)
-        end
-        file.write(v.func)
-        file.close()
+end
+local programs = textutils.unserialise(%q)
+local Dirs = textutils.unserialise(%s)
+for _,v in pairs(Dirs) do
+if Path
+    then
+        fs.makeDir(fs.combine(Path,v.Path))
+    else
+        fs.makeDir(v.Path)
+     end
+end
+for _,v in pairs(programs) do
+    local file,mess
+    if Path
+    then
+        file,mess = fs.open(fs.combine(Path,v.Path),"w")
+    else
+        file,mess = fs.open(v.Path,"w")
     end
+    if not file
+    then
+        error(mess,0)
+    end
+    file.write(v.func)
+    file.close()
+end
     
 ]]
-fm.OverWrite(LUA_WRITE,runhanndle:format(LUA_isRealitive,"%s","%s",textutils.serialise(mainTable),textutils.serialise(DirsMainTable)),"R")
-
-
+if LUA_Install and type(LUA_Install) == "boolean"
+then
+    runhanndle = runhanndle:format(true,"%s","%s",textutils.serialise(sFiles),textutils.serialise(sDir))
+else
+    runhanndle = runhanndle:format(false,"%s","%s",textutils.serialise(sFiles),textutils.serialise(sDir))
+end
+fm.OverWrite(LUA_WRITE,runhanndle,"R")
